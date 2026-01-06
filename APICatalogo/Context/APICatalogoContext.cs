e5f9a62415d930cf0f5aa5378880f6b4c249f594
Luis Silva <luissilvam537@gmail.com>
2026-01-06 11:01:15 -0300

    Defining the Entity Framework Context Class

---
+++ APICatalogo/Context/APICatalogoContext.cs
@@ -0,0 +1,13 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Context
+{
+    public class APICatalogoContext : DbContext
+    {
+        public APICatalogoContext(DbContextOptions<APICatalogoContext> options ) : base(options) { }
+
+        DbSet<Category>? Categories { get; set; }
+        DbSet<Product>? Products { get; set; }
+    }
+}
