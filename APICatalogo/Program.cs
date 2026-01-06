adbce8575ab662bb96e35a83d936ac6295a006f8
Luis Silva <luissilvam537@gmail.com>
2026-01-06 11:15:13 -0300

    Defining the connection string to the Database and injection into the DI container

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var MySQLConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<APICatalogoContext>(options =>
+options.UseMySql(MySQLConnection, 
+ServerVersion.AutoDetect(MySQLConnection)));
 
 var app = builder.Build();
 
