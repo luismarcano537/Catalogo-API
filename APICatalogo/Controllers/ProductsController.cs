c5932b696a0dba39d179b1b48757f3729fb3d763
Luis Silva <luissilvam537@gmail.com>
2026-01-08 21:50:03 -0300

    Add HttpGet by ID

--- APICatalogo/Controllers/ProductsController.cs
+++ APICatalogo/Controllers/ProductsController.cs
@@ -20,11 +20,22 @@ namespace APICatalogo.Controllers
         public ActionResult<IEnumerable<Product>> Get()
         {
             var Products = _Context.Products.ToList();
-            if (Products == null)
+            if (Products is null)
             {
                 return NotFound();
             }
             return Products;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Product> GetID(int id)
+        {
+            var Product = _Context.Products.FirstOrDefault(p => p.ProductID == id);
+            if (Product is null)
+            {
+                return NotFound();
+            }
+            return Product;
         }
     }
 }
