fa10679b7224919c4d0164507798d93c7446ed2f
Luis Silva <luissilvam537@gmail.com>
2026-01-26 21:41:48 -0300

    Creation of the IRepository interface and Repository class to implement Repository Pattern.

---
+++ APICatalogo/Repositories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace APICatalogo.Repositories;
+
+public interface IRepository<T>
+{
+    IEnumerable<T> Get();
+    T GetByID(Expression<Func<T, bool>> predicate);
+    T Create(T entity);
+    T Update(T entity);
+    T Delete(T entity);
+}
---
+++ APICatalogo/Repositories/Repository.cs
@@ -0,0 +1,52 @@
+﻿using APICatalogo.Models;
+using APICatalogo.Context;
+using System.Linq.Expressions;
+
+namespace APICatalogo.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected readonly APICatalogoContext _context;
+    public Repository(APICatalogoContext context)
+    {
+        _context = context;
+    }
+
+
+    public IEnumerable<T> Get()
+    {
+        return _context.Set<T>().ToList();
+    }
+
+
+    public T GetByID(Expression<Func<T, bool>> predicate)
+    {
+        return _context.Set<T>().FirstOrDefault(predicate);
+    }
+
+
+    public T Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+
+    public T Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+        _context.SaveChanges();
+
+        return entity;
+    }
+
+
+    public T Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+        _context.SaveChanges();
+        
+        return entity;
+    }
+}
